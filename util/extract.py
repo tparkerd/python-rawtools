6d6be99344261211047ba93e80f85f9a6f049147
Tim Parker <Tim.ParkerD@gmail.com>
2019-09-24 15:50:52 -0500

    Clean up test image output and debug statements

--- util/extract.py
+++ util/extract.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 import argparse
+import io
 import logging
 import math
 import os
 import re
+import sys
 from pprint import pformat
-import io
 
 import numpy as np
 import PIL
@@ -14,42 +15,49 @@ from PIL import Image, ImageDraw, ImageFont
 from tqdm import tqdm
 
 
-def get_slice(args, fp):
+def read_dimensions(args, fp):
   # Get its respective .DAT file to get its dimensions
   dat_fp = f'{os.path.dirname(fp)}/{"".join(os.path.splitext(os.path.basename(fp))[:-1])}.dat'
-  logging.debug(f'.DAT Path = {dat_fp}')
   # If it cannot find .DAT file for specified .RAW
   if not os.path.isfile(dat_fp) or not os.path.exists(dat_fp):
     print(f'The DAT file for {fp} cannot be found. \'{dat_fp}\' cannot be found.')
-    return
-  # Extract the resolution from .DAT file
+    sys.exit(1)
+  logging.debug(f'.DAT Path = {dat_fp}')
   with open(dat_fp, mode='r') as dfp:
     file_contents = dfp.readlines()
   file_contents = ''.join(file_contents)
   resolution_pattern = re.compile(r'\w+\:\s+(?P<x>\d+)\s+(?P<y>\d+)\s+(?P<z>\d+)')
   match = re.search(resolution_pattern, file_contents)
-  x = int(match.group('x'))
-  y = int(match.group('y'))
-  z = int(match.group('z'))
+  return int(match.group('x')), int(match.group('y')), int(match.group('z'))
+
+def get_slice(args, fp):
+  # Extract the resolution from .DAT file
+  x, y, z = read_dimensions(args, fp)
 
   # Get the requested slice index or default to the midslice on the Y axis
   if args.index is None:
     i = int(math.ceil(x / 2))
+    print(f'Slice index not specified. Using midslice as default: \'{i}\'.')
   else:
     i = args.index
-  buffer_size = x * y * np.dtype('uint16').itemsize # One slice
+  
+  # Calculate the number of bytes in a *single* slice of .RAW datafile
+  # NOTE(tparker): This assumes that a unsigned 16-bit .RAW volume
+  buffer_size = x * y * np.dtype('uint16').itemsize
 
   # Calculate the index bounds for the bytearray of a slice
+  # This byte array is a 1-D array of the image data for the current slice
+  # The index defined
+  if i < 0 or i > y - 1:
+    print(f'OutOfBoundsError - Index specified, \'{i}\' outside of dimensions of image. Image dimensions are ({x}, {y}). Slices are indexed from 0 to (N - 1)')
+    sys.exit(1)
   start_byte = (np.dtype('uint16').itemsize * x * i)
   end_byte = (np.dtype('uint16').itemsize * (x * (i + 1)) + 1)
   width = int((end_byte - start_byte - 1) / np.dtype('uint16').itemsize)
   logging.debug(f'Extract slice bounds: <{start_byte}, {end_byte}>')
   logging.debug(f'Extracted dimensions: ({width}, {1})')
-
-  logging.debug(f'Slice Index: {i}')
-  logging.debug(f'np.dtype("uint16").itemsize = {np.dtype("uint16").itemsize}')
   logging.debug(f'buffer_size = {buffer_size} (Slice size in bytes)')
-  # Target slice is (y * i) to ((y * i) - 1)
+  
   # The width of the extracted slice will be Y (2 bytes per, so 2Y bytes in length)
   logging.info(f'File Size for \'{fp}\' = {os.path.getsize(fp)} bytes')
   with open(fp, mode='rb', buffering=buffer_size) as ifp:
@@ -57,15 +65,12 @@ def get_slice(args, fp):
     iSlice = np.empty([y, z], dtype=np.uint16)
 
     byte_slice = ifp.read(buffer_size) # Byte sequence
-    start_byte 
-    test = byte_slice[(2*x*200):(2*x*300)]
-    logging.debug(f'len(test) = {len(test)}')
+    # test = byte_slice[(2*x*200):(2*x*300)]
+    # logging.debug(f'len(test) = {len(test)}')
 
     arr = np.frombuffer(byte_slice, dtype=np.uint16) # 2-byte pair sequence
-    logging.debug(f'len(ifp) = {len(byte_slice)}')
-    logging.debug(pformat(fp))
-    logging.debug(f'len(arr) = {len(arr)}')
     slice_count = 1
+    # for barr in 
     while arr.size > 0:
       # logging.debug(f'Extracting bytes arr[{start_byte}: {end_byte}]')
       ith_byte_sequence = arr[start_byte: end_byte]
@@ -84,10 +89,10 @@ def get_slice(args, fp):
     img = Image.new("I", (x,y))
     img.frombytes(array_buffer, 'raw', "I;16")
 
-    # image is (width, height)
-    imgTest = Image.new("I", (x,100))
-    imgTest.frombytes(test, 'raw', "I;16")
-    imgTest.save('test.tiff', format="tiff")
+    # # image is (width, height)
+    # imgTest = Image.new("I", (x,100))
+    # imgTest.frombytes(test, 'raw', "I;16")
+    # imgTest.save('test.tiff', format="tiff")
 
     # This is the temporary location for me to just see all the generated images
     # TODO(tparker): Change this to generate the midslices in the exact same folder as the .RAW & .DAT
@@ -97,7 +102,7 @@ def get_slice(args, fp):
     # The PNG slices seemed a lot darker than the TIFF version
 
     # output_png = f'{os.getcwd()}/{"".join(os.path.splitext(os.path.basename(fp))[:-1])}.{i}.png'
-    output_tiff = f'{os.getcwd()}/data/{"".join(os.path.splitext(os.path.basename(fp))[:-1])}.{str(i).zfill(5)}.tiff'
+    output_tiff = f'{os.getcwd()}/{"".join(os.path.splitext(os.path.basename(fp))[:-1])}.{str(i).zfill(5)}.tiff'
     # print(f'Saving Slice (ID: {i}) as {output_png}')
     # img.save(output_png)
     print(f'Saving Slice (ID: {i}) as {output_tiff}')
