969cd62c032530ca618cf15baa55f91d08819638
Tim Parker <Tim.ParkerD@gmail.com>
2020-03-18 22:46:36 -0500

    End-of-day progress, read in dummy image to convert to RAW/DAT files

--- raw_utils/raw_gen/gen-raw.py
+++ raw_utils/raw_gen/gen-raw.py
@@ -12,23 +12,21 @@ import os
 import sys
 from datetime import datetime as dt
 from multiprocessing import Pool, Value, cpu_count
-from pprint import pprint
 
-import unittest
 import numpy as np
 from PIL import Image
-from scipy import misc, ndimage
 from tqdm import tqdm
 
-from raw_utils.core.metadata import write_dat, read_dat 
+from raw_utils.core.metadata import read_dat, write_dat
+
 
 def generate_volume(args):
-  
+
   # First slice
   i, j, k = [250,250,250]
   t = (2 ** 16) - 1
   data = np.zeros(shape=(i,j,k), dtype=np.uint16)
-  
+
   # T
   data[100:150, 0:10, 115:135] = t
   data[120:130, 10:60, 115:135] = t
@@ -47,11 +45,10 @@ def generate_volume(args):
   # T
   data[100:150, 160:170, 115:135] = t
   data[120:130, 170:220, 115:135] = t
-  
+
 
   data = np.rot90(data, k=1, axes=(1,0)) # rotate to match one of Adam's roots
   return data
-
 
 def options():
     parser = argparse.ArgumentParser(description='Convert .raw 3d volume file to typical image format slices',formatter_class=argparse.ArgumentDefaultsHelpFormatter)
@@ -60,7 +57,7 @@ def options():
     parser.add_argument("-V", "--version", action="version", version='%(prog)s 1.1.0')
     parser.add_argument('-t', "--threads", type=int, default=cpu_count(), help=f"Maximum number of threads dedicated to processing.")
     parser.add_argument('--force', action="store_true", help="Force file creation. Overwrite any existing files.")
-    # parser.add_argument("path", metavar='PATH', type=str, nargs='+', help='List of directories to process')
+    parser.add_argument("path", metavar='PATH', type=str, nargs='+', help='Image filepath(s)')
     args = parser.parse_args()
 
     # Configure logging, stderr and file logs
@@ -86,41 +83,70 @@ def options():
 
     return args
 
+def process(args, fp):
+    with open(fp, 'rb') as ifp:
+        img = Image.open(ifp).convert('LA')
+    # Convert to numpy array
+    img_xs = np.array(img).astype('float64')
+    logging.debug(img_xs.shape)
+    img.show()
+    print(img_xs)
+    img_xs *= (2 ** 16 - 1)/img_xs.max()
+    img_xs = img_xs.astype('uint16')
+    print(img_xs)
+    
+    # TODO(tparker): Convert a flat image into a 3-D numpy array that can
+    # be converte into a .RAW and write a .DAT
+
+
 if __name__ == "__main__":
     args = options()
-    fp = os.path.join(os.getcwd(), 'test.dat')
-    dims = (250, 250, 300)
-    thickness = (0.1, 0.1, 0.1)
-    # Valid case: (Tuples)
-    # write_dat(fp, dimensions = (150, 150, 240), thickness = (0.1, 0.1, 0.1), dtype='uint16')
-    
-    # # Valid case: (Dicts)
-    # write_dat(fp, dimensions = {'x': 160, 'y': 160, 'z': 250}, thickness = {'x': 0.11, 'y': 0.11, 'z': 0.11}, dtype='uint16')
-    
-    # # Invalid case: dimensions is not a dict, tuple, or list
-    # # write_dat(fp, dimensions = 1124, thickness = tuple([0.1444]*3))
-    
-    # # Invalid case: missing a dimension
-    # # write_dat(fp, dimensions = (1499, 1499), thickness = (0.2,0.2,0.2))
-    
-    # # Invalid case: dict missing value
-    # # write_dat(fp, dimensions = {'x': 1499, 'y': 1499}, thickness = tuple([0.1444]*3))
-    
-    # # Invalid case: invalid input type for dimensions
-    # write_dat(fp, ('2343', 3234, 2343), thickness)
-    
-    # # Valid case: list of dims
-    # write_dat(fp, list(dims), thickness)
-    
-    # # Valid case: 8-bit volume
-    # write_dat(fp, dims, thickness, dtype='uint8')
-    # # Valid case: 8-bit volume
-    # write_dat(fp, dims, thickness, dtype='float32')
-    
-    read_dat(fp)
-    
-    # Later, determine if being piped out and then send to a file via pipe
-    # if sys.stdout.isatty() is False: # then IS being piped out
-    # print(sys.stdout.isatty())
-    # with open('testvolume.raw', 'wb') as ofp:
-    #   generate_volume(args).tofile(ofp)
+    for fp in [ os.path.abspath(fp) for fp in args.path ]:
+        logging.info(f"Processing: '{fp}'")
+        process(args, fp)
+
+
+
+
+
+
+
+    # fp = os.path.join(os.getcwd(), 'test.dat')
+    # dims = (250, 250, 300)
+    # thickness = (0.1, 0.1, 0.1)
+
+
+
+    # # Valid case: (Tuples)
+    # # write_dat(fp, dimensions = (150, 150, 240), thickness = (0.1, 0.1, 0.1), dtype='uint16')
+
+    # # # Valid case: (Dicts)
+    # # write_dat(fp, dimensions = {'x': 160, 'y': 160, 'z': 250}, thickness = {'x': 0.11, 'y': 0.11, 'z': 0.11}, dtype='uint16')
+
+    # # # Invalid case: dimensions is not a dict, tuple, or list
+    # # # write_dat(fp, dimensions = 1124, thickness = tuple([0.1444]*3))
+
+    # # # Invalid case: missing a dimension
+    # # # write_dat(fp, dimensions = (1499, 1499), thickness = (0.2,0.2,0.2))
+
+    # # # Invalid case: dict missing value
+    # # # write_dat(fp, dimensions = {'x': 1499, 'y': 1499}, thickness = tuple([0.1444]*3))
+
+    # # # Invalid case: invalid input type for dimensions
+    # # write_dat(fp, ('2343', 3234, 2343), thickness)
+
+    # # # Valid case: list of dims
+    # # write_dat(fp, list(dims), thickness)
+
+    # # # Valid case: 8-bit volume
+    # # write_dat(fp, dims, thickness, dtype='uint8')
+    # # # Valid case: 8-bit volume
+    # # write_dat(fp, dims, thickness, dtype='float32')
+
+    # read_dat(fp)
+
+    # # Later, determine if being piped out and then send to a file via pipe
+    # # if sys.stdout.isatty() is False: # then IS being piped out
+    # # print(sys.stdout.isatty())
+    # # with open('testvolume.raw', 'wb') as ofp:
+    # #   generate_volume(args).tofile(ofp)
