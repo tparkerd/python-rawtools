9c218dfbc59a033c5acbc2ebe6863c3e6bc2eed4
Tim <tim.parkerd@gmail.com>
2021-04-25 21:41:59 -0500

    Fixed code styling

--- rawtools/crop.py
+++ rawtools/crop.py
@@ -63,34 +63,41 @@ def process(fp, *args, **kwargs):
     else:
         end = z
 
+
+    # Collate information about subsetted volume
     subset_volumes = {}
     total_slices = 0
     for co in cutoffs:
+        subset_volumes[co] = {}
+        # Volume bounds
+        start = kwargs[co]
+        subset_volumes[co]["start"] = start
+        subset_volumes[co]["end"] = end
+        # File paths
         cropped_ofpath = f"{prefix}_{co}{ext}"
         cropped_ofpath = os.path.join(gargs.outdir, cropped_ofpath)
         cropped_dat_ofpath = f"{prefix}_{co}.dat"
         cropped_dat_ofpath = os.path.join(gargs.outdir, cropped_dat_ofpath)
-        logging.debug(f"{cropped_ofpath=}")
-        start = kwargs[co]
-        # Collate information about subset volume
-        subset_volumes[co] = {}
         subset_volumes[co]["ofpath"] = cropped_ofpath
-        subset_volumes[co]["start"] = start
-        subset_volumes[co]["end"] = end
         subset_volumes[co]["dat_ofpath"] = cropped_dat_ofpath
-        logging.info(f"Export '{co}': {start} to {end}")
-        logging.debug(pformat(subset_volumes[co]))
-        dat.write(cropped_dat_ofpath, (x, y, end - start + 1), (xth, yth, zth))
-        total_slices += end - start + 1
+
+        logging.debug(f"Export '{co}': {start} to {end}")
+        logging.debug(f"{cropped_ofpath=}")
+
+        # Write DAT file
+        dat.write(cropped_dat_ofpath, (x, y, end - start), (xth, yth, zth))
+
+        # Keep track of the total iterations needed to process the volume
+        total_slices += (end - start + 1)
 
     # Open file handlers
-    logging.info(f"Opening input data: '{fp}'")
+    logging.debug(f"Opening input data: '{fp}'")
     with open(fp, "rb") as ifp:
         description = f"Extracting data from '{os.path.basename(fp)}' ({bitdepth})"
         pbar_position = None
         if "pbar_position" in kwargs:
             pbar_position = kwargs["pbar_position"]
-            logging.info(f"'{pbar_position=}'")
+            logging.debug(f"'{pbar_position=}'")
         pbar = tqdm(total=total_slices, desc=description, position=pbar_position)
         # For each subset, export the slice if within bounds
         for key in subset_volumes.keys():
@@ -101,7 +108,8 @@ def process(fp, *args, **kwargs):
             [subset_volumes[key]["start"] for key in subset_volumes.keys()]
         )
         logging.debug(f"{earliest_included_slice=}")
-        for i in range(earliest_included_slice, end + 1):
+        logging.debug(f"Loading slices [{earliest_included_slice}, {end}]")
+        for i in range(earliest_included_slice, end + 1): # range cuts off 1 less than end
             ifp.seek(i * offset)
             chunk = np.fromfile(ifp, dtype=bitdepth, count=img_size, sep="")
             for key in subset_volumes.keys():
@@ -109,15 +117,17 @@ def process(fp, *args, **kwargs):
                     logging.debug(
                         f"Write '{i}' data to '{subset_volumes[key]['ofpath']}'"
                     )
-                    chunk.tofile(subset_volumes[key]["ofp"])
-                pbar.update(1)
+                    chunk.tofile(subset_volumes[key]["ofp"]) # comment to speed up
+                    pbar.update(1)
+
+        if pbar is not None:
+            pbar.close()
+            pbar = None
 
         # Finished processing data, close up output files
         for key in subset_volumes.keys():
+            logging.debug(f"Closing '{subset_volumes[key]['ofpath']}'")
             subset_volumes[key]["ofp"].close()
-
-            pbar.close()
-            pbar = None
 
 
 def crop_by_node(args):
@@ -154,6 +164,7 @@ def crop_by_node(args):
         scan_uids = list(scan_uids)
         logging.debug(f"{scan_uids=}")
 
+        # Set index to the UID
         df.set_index("UID", inplace=True)
 
         # Create dictionary to collate input and output files for each UID
@@ -166,8 +177,9 @@ def crop_by_node(args):
         for uid in scan_uids:
             # find the associated RAW file
             for root, dirs, files in os.walk(gargs.path):
-                for fp in files:
-                    if fp.endswith(".raw") and fp.split("_")[3] == uid:
+                raw_files = [ f for f in files if f.endswith('.raw') ]
+                for fp in raw_files:
+                    if fp.split("_")[3] == uid:
                         scans[uid]["fp"] = os.path.join(root, fp)
 
             # Get slice index for each node of the volume
@@ -184,6 +196,9 @@ def crop_by_node(args):
         logging.debug(scans)
         for uid in scans.keys():
             scan = scans[uid]
+            if "fp" not in scan:
+                logging.warning(f"No input file found for '{uid}'")
+                continue
             fp = scan["fp"]
             nodes = scan["nodes"]
             end = scan["end"]
