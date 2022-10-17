85fb963608c09448b9831f4c2d3321a068cd7999
Tim Parker <Tim.ParkerD@gmail.com>
2022-10-16 22:35:12 -0500

    Bump version: 0.5.0 → 0.6.0

--- setup.py
+++ setup.py
@@ -67,6 +67,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/Topp-Roots-Lab/python-rawtools',
-    version='0.5.0',
+    version='0.6.0',
     zip_safe=False,
 )
