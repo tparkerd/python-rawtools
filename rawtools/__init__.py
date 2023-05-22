c9929335261ab1557180ca8fa575f42ebea7d85b
Tim Parker <Tim.ParkerD@gmail.com>
2023-05-22 00:22:19 -0500

    add convenience import for read_raw

--- rawtools/__init__.py
+++ rawtools/__init__.py
@@ -4,6 +4,8 @@ from __future__ import annotations
 from importlib.metadata import PackageNotFoundError
 from importlib.metadata import version
 
+from rawtools.convert.image.raw import read_raw  # noqa: F401
+
 try:
     __version__ = version(__package__)
 except PackageNotFoundError:
