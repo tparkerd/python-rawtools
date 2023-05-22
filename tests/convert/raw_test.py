aba5da6553d35a7ae9d3d85dc0d87b366deb5ed0
Tim Parker <Tim.ParkerD@gmail.com>
2023-05-22 00:14:44 -0500

    test: add unit tests for raw conversion

--- tests/convert/raw_test.py
+++ tests/convert/raw_test.py
@@ -1,14 +1,18 @@
 from __future__ import annotations
 
 import os
-from functools import reduce
+import random
+from math import prod
 from pathlib import Path
+from textwrap import dedent
 
 import numpy as np
 import pytest
 import raster_geometry as rg
 
+from rawtools.convert.image import raw
 from rawtools.convert.image.raw import Raw
+from rawtools.utils import dat
 from rawtools.utils.dataset import Dataset
 
 
@@ -56,18 +60,6 @@ def valid_dataset(fs):
     yield dataset
 
 
-def test_raw_create_raw(fs):
-    fname = '2023_Universe_test-data_valid'
-    raw_src = Path('tests', 'data', 'image', 'pristine', f'{fname}.raw')
-    dat_src = Path('tests', 'data', 'image', 'pristine', f'{fname}.dat')
-    raw_dest = Path('data', f'{fname}.raw').absolute()
-    dat_dest = Path('data', f'{fname}.dat').absolute()
-    fs.add_real_file(raw_src, target_path=raw_dest)
-    fs.add_real_file(dat_src, target_path=dat_dest)
-    raw = Raw(raw_dest)
-    assert raw
-
-
 def test_raw_fetch_dimensions(valid_raw, expected_raw):
     assert valid_raw.dims == expected_raw['dims']
 
@@ -110,7 +102,7 @@ def test_raw_read_approx_dat_filepath_match(caplog, fs):
 
 
 def test_raw_expected_filesize(valid_raw, expected_raw):
-    expected_filesize = reduce(lambda x, y: x * y, expected_raw['dims']) * 2
+    expected_filesize = prod(expected_raw['dims']) * 2
     assert valid_raw.filesize == expected_filesize
 
 
@@ -128,7 +120,7 @@ def test_raw_expected_filesize(valid_raw, expected_raw):
 )
 def test_raw_to_slices(ext, bitdepth, tmp_path):
     fname = '2020_Universe_Example_foo'
-    dims = (200, 300, 400)
+    dims = (random.randrange(100, 200), random.randrange(200, 300), random.randrange(300, 400))
     x, y, z = dims
     dtype = 'uint16'
 
@@ -138,27 +130,189 @@ def test_raw_to_slices(ext, bitdepth, tmp_path):
 
     dat_contents = f"""\
     ObjectFileName: {fname}.raw
-    Resolution:     200 300 400
+    Resolution:     {' '.join([str(x) for x in dims])}
     SliceThickness: 0.123456 0.123456 0.123456
     Format:         USHORT
     ObjectModel:    DENSITY
     """
     target_dat_fpath.write_text(dat_contents)
 
-    # Create dummy data with a floating sphere
+    # Create dummy data with a floating cube
     voxel_values = (2**(np.dtype(dtype).itemsize * 8) - 1)  # brightest value
     side_length = 75
     raw_data = (
         rg.cube(shape=(z, y, x), side=side_length, position=0.5)
         .astype(np.dtype(dtype)) * voxel_values
     )
-    raw_data_bytes = raw_data.tobytes()
-    target_raw_fpath.write_bytes(raw_data_bytes)
+    target_raw_fpath.write_bytes(raw_data.tobytes())
     raw = Raw(target_raw_fpath)
 
-    raw.to_slices(fpath=target_slices_path, ext=ext, dtype=bitdepth)
+    raw.to_slices(ext=ext, bitdepth=bitdepth)
 
     assert target_raw_fpath.exists()
     assert target_dat_fpath.exists()
     assert target_slices_path.exists()
     assert len(os.listdir(target_slices_path)) == z
+
+
+@pytest.mark.parametrize(
+    'ext', [
+        'png', 'tif',
+    ],
+)
+@pytest.mark.parametrize(
+    'bitdepth', [
+        'uint8', 'uint16',
+    ],
+)
+def test_raw_to_slices_batch(ext, bitdepth, tmp_path):
+
+    def __generate_raw():
+        uid = f'{random.randrange(100,500)}-{random.randrange(1,10)}'
+        fname = f'2020_Universe_Example_{uid}'
+        dims = (random.randrange(100, 200), random.randrange(200, 300), random.randrange(300, 400))
+        x, y, z = dims
+        dtype = 'uint16'
+
+        target_raw_fpath = tmp_path / f'{fname}.raw'
+        target_dat_fpath = tmp_path / f'{fname}.dat'
+
+        dat_contents = f"""\
+        ObjectFileName: {fname}.raw
+        Resolution:     {' '.join([str(x) for x in dims])}
+        SliceThickness: 0.123456 0.123456 0.123456
+        Format:         USHORT
+        ObjectModel:    DENSITY
+        """
+        target_dat_fpath.write_text(dat_contents)
+
+        # Create dummy data with a floating cube
+        voxel_values = (2**(np.dtype(dtype).itemsize * 8) - 1)  # brightest value
+        side_length = 75
+        raw_data = (
+            rg.cube(shape=(z, y, x), side=side_length, position=0.5)
+            .astype(np.dtype(dtype)) * voxel_values
+        )
+        target_raw_fpath.write_bytes(raw_data.tobytes())
+        r = Raw(target_raw_fpath)
+        return r
+
+    samples = [__generate_raw() for _ in range(3)]
+    target_output_path = tmp_path / 'output'
+    raw.batch_convert(*samples, target_directory=target_output_path)
+
+
+@pytest.mark.parametrize(
+    'input_bitdepth', [
+        'uint8', 'uint16', 'float32',
+    ],
+)
+@pytest.mark.parametrize(
+    'output_bitdepth', [
+        'uint8', 'uint16', 'float32',
+    ],
+)
+def test_raw_to_raw(input_bitdepth, output_bitdepth, tmp_path):
+
+    def __generate_raw():
+        uid = f'{random.randrange(100,500)}-{random.randrange(1,10)}'
+        fname = f'2020_Universe_Example_{uid}'
+        dims = (random.randrange(100, 200), random.randrange(200, 300), random.randrange(300, 400))
+        x, y, z = dims
+        bitdepth = input_bitdepth
+
+        target_raw_fpath = tmp_path / f'{fname}.raw'
+        target_dat_fpath = tmp_path / f'{fname}.dat'
+
+        dat_contents = f"""\
+        ObjectFileName: {fname}.raw
+        Resolution:     {' '.join([str(x) for x in dims])}
+        SliceThickness: 0.123456 0.123456 0.123456
+        Format:         {dat.format_from_bitdepth(bitdepth)}
+        ObjectModel:    DENSITY
+        """
+        dat_contents = dedent(dat_contents)
+        target_dat_fpath.write_text(dat_contents)
+
+        # Create dummy data with a floating cube
+        voxel_values = (2**(np.dtype(bitdepth).itemsize * 8) - 1)  # brightest value
+        side_length = 75
+        raw_data = (
+            rg.cube(shape=(z, y, x), side=side_length, position=0.5) * voxel_values
+        ).astype(np.dtype(bitdepth))
+        target_raw_fpath.write_bytes(raw_data.tobytes())
+        r = Raw(target_raw_fpath)
+        return r
+
+    r = __generate_raw()
+    expected_filesize = prod(r.dims) * np.dtype(output_bitdepth).itemsize
+
+    output_fpath = tmp_path / 'foo.raw'
+    r.to_raw(output_fpath, bitdepth=output_bitdepth)
+    new_r = Raw(output_fpath)
+    assert os.stat(output_fpath).st_size == expected_filesize
+    assert new_r
+
+
+@pytest.mark.skip(reason='Requires substantial amount of RAM to scale a .raw')
+@pytest.mark.slow
+@pytest.mark.parametrize(
+    'input_bitdepth', [
+        'uint8',
+        'uint16',
+        'float32',
+    ],
+)
+@pytest.mark.parametrize(
+    'output_bitdepth', [
+        'uint8',
+        'uint16',
+        'float32',
+    ],
+)
+def test_raw_to_raw_reshape(input_bitdepth, output_bitdepth, tmp_path):
+
+    def __generate_raw():
+        uid = f'{random.randrange(100,500)}-{random.randrange(1,10)}'
+        fname = f'2020_Universe_Example_{uid}'
+        dims = (random.randrange(100, 200), random.randrange(200, 300), random.randrange(300, 400))
+        x, y, z = dims
+        bitdepth = input_bitdepth
+
+        target_raw_fpath = tmp_path / f'{fname}.raw'
+        target_dat_fpath = tmp_path / f'{fname}.dat'
+
+        dat_contents = f"""\
+        ObjectFileName: {fname}.raw
+        Resolution:     {' '.join([str(x) for x in dims])}
+        SliceThickness: 0.123456 0.123456 0.123456
+        Format:         {dat.format_from_bitdepth(bitdepth)}
+        ObjectModel:    DENSITY
+        """
+        dat_contents = dedent(dat_contents)
+        target_dat_fpath.write_text(dat_contents)
+
+        # Create dummy data with a floating sphere
+        brightest_value = (2**(np.dtype(bitdepth).itemsize * 8) - 1)  # brightest value
+        raw_data = (rg.sphere(radius=25, shape=(z, y, x), position=0.5) * brightest_value).astype(bitdepth)
+        raw_bytes = raw_data.tobytes()
+        # target_raw_fpath.write_bytes(raw_bytes)
+        with open(target_raw_fpath, 'wb') as ofp:
+            ofp.write(raw_bytes)
+        r = Raw(target_raw_fpath)
+        return r
+
+    r = __generate_raw()
+    new_dims = (r.dims[0], r.dims[1], r.dims[2] // 2)
+    print(f'{new_dims=}')
+    expected_filesize = prod(new_dims) * np.dtype(output_bitdepth).itemsize
+
+    fname = 'foo.raw'
+    output_fpath = tmp_path / fname
+    r.to_raw(output_fpath, bitdepth=output_bitdepth, shape=new_dims)
+    new_r = Raw(output_fpath)
+    assert new_r
+    assert os.stat(output_fpath).st_size == expected_filesize
+
+
+# TODO: check if changing the output_directory for to_slices() works as intended
--- tests/rawtools_test.py
+++ tests/rawtools_test.py
@@ -1,109 +1,86 @@
-#!/usr/bin/env python
-"""Tests for `rawtools` package."""
+# #!/usr/bin/env python
+# """Tests for `rawtools` package."""
+# from __future__ import annotations
+# import numpy as np
+# import pytest
+# from numpy import uint16
+# from numpy import uint8
+# DIMS = (4, 5)
+# @pytest.fixture
+# def slice_uint8():
+#     """Sample uint8 slice"""
+#     return np.rint(np.arange(0, 20, dtype=uint8).reshape(DIMS))
+# @pytest.fixture
+# def slice_uint16():
+#     """Sample uint16 slice"""
+#     return np.rint(np.arange(0, 20, dtype=uint16).reshape(DIMS))
+# @pytest.fixture
+# def slice_uint16_high_variance():
+#     """Sample uint16 slice with variable values"""
+#     return np.array(
+#         [-1, 0, 100, 1000, 5000, 14830, 50321, 65535, 65536],
+#     ).astype(dtype=uint16)
+# @pytest.fixture
+# def dat_files():
+#     """Sample .dat files' paths"""
+#     return [
+#         'testing/resources/ideal_dragonfly.dat',
+#         'testing/resources/ideal_nsi.dat',
+#         'testing/resources/poor_dragonfly.dat',
+#         'testing/resources/poor_nsi.dat',
+#     ]
+# def test_scale_uint8(slice_uint8):
+#     """Test scaling a unsigned 8-bit integer array to own bounds."""
+#     from rawtools.convert import scale
+#     xs = np.arange(0, 20, dtype=uint8).reshape(DIMS)
+#     lbound = np.iinfo(uint8).min
+#     ubound = np.iinfo(uint8).max
+#     scaled_slice = scale(xs, lbound, ubound, lbound, ubound)
+#     np.testing.assert_array_equal(scaled_slice, slice_uint8)
+# def test_scale_uint16_to_uint8(slice_uint16):
+#     """Test scaling an unsigned 16-bit integer array to an unsigned 8-bit
+#     array's bounds.
+#     """
+#     from rawtools.convert import scale
+#     lbound = np.iinfo(uint16).min
+#     ubound = np.iinfo(uint16).max
+#     new_lbound = np.iinfo(uint8).min
+#     new_ubound = np.iinfo(uint8).max
+#     slice_uint8 = np.zeros(DIMS, dtype=uint8)
+#     scaled_slice = np.rint(
+#         scale(slice_uint16, lbound, ubound, new_lbound, new_ubound),
+#     )
+#     np.testing.assert_array_equal(scaled_slice, slice_uint8)
+# def test_scale_uint16_to_uint8_large_variance(slice_uint16_high_variance):
+#     """Test scaling an unsigned 16-bit integer array with high variance to an
+#     unsigned 8-bit array's bounds.
+#     """
+#     from rawtools.convert import scale
+#     lbound = np.iinfo(uint16).min
+#     ubound = np.iinfo(uint16).max
+#     new_lbound = np.iinfo(uint8).min
+#     new_ubound = np.iinfo(uint8).max
+#     # Mapped values should wrap as they exceed target bit depth
+#     slice_uint8 = np.array([255, 0, 0, 4, 19, 58, 196, 255, 0], dtype=uint8)
+#     scaled_slice = np.rint(
+#         scale(
+#             slice_uint16_high_variance,
+#             lbound,
+#             ubound,
+#             new_lbound,
+#             new_ubound,
+#         ),
+#     )
+#     np.testing.assert_array_equal(scaled_slice, slice_uint8)
+# def test_dat_read_both_formats(dat_files):
+#     """Test dat.read() on 4 example .dat (two acceptable and two unacceptable)
+#     covering both Dragonfly and NSI formats"""
+#     from rawtools.convert.text.dat import read
+#     # neither of these should raise any errors
+#     read(dat_files[0])
+#     read(dat_files[1])
+#     with pytest.raises(ValueError, match=r'Unable to parse'):
+#         read(dat_files[2])
+#     with pytest.raises(ValueError, match=r'Unable to parse'):
+#         read(dat_files[3])
 from __future__ import annotations
-
-import numpy as np
-import pytest
-from numpy import uint16
-from numpy import uint8
-
-DIMS = (4, 5)
-
-
-@pytest.fixture
-def slice_uint8():
-    """Sample uint8 slice"""
-    return np.rint(np.arange(0, 20, dtype=uint8).reshape(DIMS))
-
-
-@pytest.fixture
-def slice_uint16():
-    """Sample uint16 slice"""
-    return np.rint(np.arange(0, 20, dtype=uint16).reshape(DIMS))
-
-
-@pytest.fixture
-def slice_uint16_high_variance():
-    """Sample uint16 slice with variable values"""
-    return np.array(
-        [-1, 0, 100, 1000, 5000, 14830, 50321, 65535, 65536],
-    ).astype(dtype=uint16)
-
-
-@pytest.fixture
-def dat_files():
-    """Sample .dat files' paths"""
-    return [
-        'testing/resources/ideal_dragonfly.dat',
-        'testing/resources/ideal_nsi.dat',
-        'testing/resources/poor_dragonfly.dat',
-        'testing/resources/poor_nsi.dat',
-    ]
-
-
-def test_scale_uint8(slice_uint8):
-    """Test scaling a unsigned 8-bit integer array to own bounds."""
-    from rawtools.convert import scale
-
-    xs = np.arange(0, 20, dtype=uint8).reshape(DIMS)
-    lbound = np.iinfo(uint8).min
-    ubound = np.iinfo(uint8).max
-    scaled_slice = scale(xs, lbound, ubound, lbound, ubound)
-    np.testing.assert_array_equal(scaled_slice, slice_uint8)
-
-
-def test_scale_uint16_to_uint8(slice_uint16):
-    """Test scaling an unsigned 16-bit integer array to an unsigned 8-bit
-    array's bounds.
-    """
-    from rawtools.convert import scale
-
-    lbound = np.iinfo(uint16).min
-    ubound = np.iinfo(uint16).max
-    new_lbound = np.iinfo(uint8).min
-    new_ubound = np.iinfo(uint8).max
-    slice_uint8 = np.zeros(DIMS, dtype=uint8)
-    scaled_slice = np.rint(
-        scale(slice_uint16, lbound, ubound, new_lbound, new_ubound),
-    )
-
-    np.testing.assert_array_equal(scaled_slice, slice_uint8)
-
-
-def test_scale_uint16_to_uint8_large_variance(slice_uint16_high_variance):
-    """Test scaling an unsigned 16-bit integer array with high variance to an
-    unsigned 8-bit array's bounds.
-    """
-    from rawtools.convert import scale
-
-    lbound = np.iinfo(uint16).min
-    ubound = np.iinfo(uint16).max
-    new_lbound = np.iinfo(uint8).min
-    new_ubound = np.iinfo(uint8).max
-    # Mapped values should wrap as they exceed target bit depth
-    slice_uint8 = np.array([255, 0, 0, 4, 19, 58, 196, 255, 0], dtype=uint8)
-    scaled_slice = np.rint(
-        scale(
-            slice_uint16_high_variance,
-            lbound,
-            ubound,
-            new_lbound,
-            new_ubound,
-        ),
-    )
-
-    np.testing.assert_array_equal(scaled_slice, slice_uint8)
-
-
-def test_dat_read_both_formats(dat_files):
-    """Test dat.read() on 4 example .dat (two acceptable and two unacceptable)
-    covering both Dragonfly and NSI formats"""
-    from rawtools.dat import read
-
-    # neither of these should raise any errors
-    read(dat_files[0])
-    read(dat_files[1])
-    with pytest.raises(ValueError, match=r'Unable to parse'):
-        read(dat_files[2])
-    with pytest.raises(ValueError, match=r'Unable to parse'):
-        read(dat_files[3])
